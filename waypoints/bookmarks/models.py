a1f73649fa13f308e706d4c09192fb5d3194bc18
Brian Wilson <brian.wilson@cgi.com>
2016-04-20 16:54:15 +0100

    Add initial bookmark model

--- waypoints/bookmarks/models.py
+++ waypoints/bookmarks/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    url = models.URLField(max_length=255)
+    title = models.CharField(max_length=255)
+    saved = models.DateTimeField(auto_now_add=True)
+    deleted = models.BooleanField(default=False)
+    comment = models.TextField(blank=True)
+    tags = models.ManyToManyField('wptags.Tag')
+
+    def __str__(self):
+        return self.title
