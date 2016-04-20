9049ba0d94861d266c27c23c0276a3b822a15863
Brian Wilson <brian.wilson@cgi.com>
2016-04-20 12:29:54 +0100

    Add initial Tag model

--- waypoints/wptags/models.py
+++ waypoints/wptags/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=255)
+    occurrences = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{tag} ({user})'.format(
+            tag=self.name,
+            user=self.user.username
+        )
