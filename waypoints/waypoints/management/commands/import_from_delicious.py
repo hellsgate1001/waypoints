ecf61613582c019ee7dbe8a4f7805b200749e42e
Brian Wilson <brian.wilson@cgi.com>
2016-04-22 12:31:47 +0100

    Finalise the HTML import

--- waypoints/waypoints/management/commands/import_from_delicious.py
+++ waypoints/waypoints/management/commands/import_from_delicious.py
@@ -9,13 +9,20 @@ from wptags.models import Tag
 
 
 class Command(BaseCommand):
+    '''
+    Parse an HTML export from delicious.com, loading the resultant bookmarks
+    and tags into a database
+    '''
     help = 'Import bookmarks and tags from the HTML export from delicious.com'
 
-    def handle(self, *args, **options):
-        user = get_user_model().objects.get(pk=1)
-        # Load the export html into a beautiful soup object
-        soup = BeautifulSoup(open('delicious.html'))
+    def add_arguments(self, parser):
+        '''
+        Parse command line arguments
+        '''
+        super(Command, self).add_arguments(parser)
 
+    @classmethod
+    def handle(self, *args, **options):
         '''
         The layout of the export puts links into <dt> tags and any related
         comments into a <dd> tag immediately following.
@@ -23,8 +30,18 @@ class Command(BaseCommand):
         Ignore all other tags
         '''
 
+        user = get_user_model().objects.get(pk=1)
+        # Load the export html into a beautiful soup object
+        soup = BeautifulSoup(open('delicious.html'))
+
+        print 'Parsing export with %s rows' % len(soup.dl.contents)
         for i in range(len(soup.dl.contents)):
             if soup.dl.contents[i].name == 'dt':
+                print(
+                    '{row}: Creating new bookmark for {url}'.format(
+                        row=i, url=soup.dl.contents[i].a['href']
+                    )
+                )
                 bookmark = Bookmark(
                     user=user,
                     url=soup.dl.contents[i].a['href'],
@@ -36,11 +53,34 @@ class Command(BaseCommand):
                         timezone.get_current_timezone()
                     )
                 )
-                if soup.dl.contents[(i + 1)].name == 'dd':
-                    bookmark.comment = soup.dl.contents[(i + 1)].string
+                print(
+                    '    {row}: Bookmark title: {title}'.format(
+                        row=i,
+                        title=soup.dl.contents[i].a.text.encode('utf-8')
+                    )
+                )
+
+                try:
+                    # Check for any comments for this bookmark
+                    if soup.dl.contents[(i + 1)].name == 'dd':
+                        print(
+                            '    {row}: Got comments: {comments}'.format(
+                                row=i,
+                                comments=soup.dl.contents[(i + 1)].string
+                            )
+                        )
+                        bookmark.comment = soup.dl.contents[(i + 1)].string
+                except IndexError:
+                    # We've reached the end of the export file
+                    pass
                 bookmark.save()
                 # Add the tags
                 for tag in soup.dl.contents[i].a['tags'].split(','):
+                    print(
+                        '        {row}: Found tag {tag}'.format(
+                            row=i, tag=tag
+                        )
+                    )
                     if tag != '':
                         try:
                             the_tag = Tag.objects.get(name=tag)
